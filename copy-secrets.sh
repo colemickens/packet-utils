1abe9f7de40255cd19f115112bba2878e2bab009
Cole Mickens <cole.mickens@gmail.com>
2018-10-07 20:42:11 -0700

    update

--- copy-secrets.sh
+++ copy-secrets.sh
@@ -7,6 +7,8 @@ rsync -avzh /etc/nixos/secrets/ root@${target}:/etc/nixos/secrets
 ssh root@${target} chown -R cole:cole /etc/nixcfg
 ssh root@${target} chown -R cole:cole /etc/nixpkgs
 
+exit 0
+
 # TODO: move this to nixcfg/utils probably... that would make a lot more sesne
 # and then call sudo -u cole /nixcfg/utils/blah
 ssh root@${target} git clone https://github.com/colemickens/cri-tools.git /home/cole/cri-tools
--- q-create.sh
+++ q-create.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 
-FACILITY=ewr1 \
+#FACILITY=ewr1 \
+#FACILITY=nrt1 \
+#FACILITY=sjc1 \
+
+#PLAN="c2.medium.x86" \
+#PLAN="m2.xlarge.x86" \
+
+#SPOT_PRICE_MAX=0.2 \
+#SPOT_PRICE_MAX=0.4 \
+
+FACILITY=sjc1 \
 PLAN="c2.medium.x86" \
 HOURS=12 \
-SPOT_PRICE_MAX=0.2 \
+SPOT_PRICE_MAX=0.21 \
 INIT=/etc/nixcfg/utils/bootstrap-configuration.nix \
   ./packet.sh device_create "${NAME:-"kix.cluster.lol"}"
 

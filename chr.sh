ca0267a2b4bd650d7637e2d0ee448b8476e9ba7b
Cole Mickens <cole.mickens@gmail.com>
2019-03-22 00:50:22 +0000

    chr.sh update

--- chr.sh
+++ chr.sh
@@ -5,16 +5,21 @@ o+=("--option" "binary-caches" "https://cache.nixos.org https://nixpkgs-wayland.
 o+=("--option" "substituters" "https://cache.nixos.org https://nixpkgs-wayland.cachix.org https://colemickens.cachix.org")
 o+=("--option" "trusted-public-keys" "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA= colemickens.cachix.org-1:oIGbn9aolUT2qKqC78scPcDL6nz7Npgotu644V4aGl4=")
 
-export url='https://github.com/colemickens/nixpkgs/archive/chromium-upstream.tar.gz'
+vaapiCommit="chromium-upstream"
+vaapiCommit="3faa77875f9d96956797737cbc8271fb0d71c382"
+export url="https://github.com/colemickens/nixpkgs/archive/${vaapiCommit}.tar.gz"
 nix-build \
   "${o[@]}" \
   "${url}" \
   -A chromium -A chromiumBeta -A chromiumDev \
     | cachix push colemickens
 
-export url='https://github.com/colemickens/nixpkgs/archive/chromium-upstream-ozone.tar.gz'
+ozoneCommit="chromium-upstream-ozone"
+ozoneCommit="cadecb09b1685436a084d8b52977d3495fc9bb60"
+export url="https://github.com/colemickens/nixpkgs/archive/${ozoneCommit}.tar.gz"
 nix-build \
   "${o[@]}" \
-   -E "with (import (builtins.fetchTarball \"${url}\") {});  chromium.override{ channel=\"dev\"; useVaapi=false; useOzone=true;}" \
-     | cachix push colemickens
+  "${url}" \
+  -A chromiumOzone \
+    | cachix push colemickens
 
